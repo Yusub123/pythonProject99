230812e36477bab6c0bbc98dcf58fa8a3d9e2080
Юсубов Юсуб Махмуд <Usub_jg9z@itstep.edu.az>
2023-03-15 15:59:41 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import turtle
+
+turtle.shape()
+
+turtle.pensize(10)
+
+turtle.forward(100)
+
+turtle.left(90)
+
+turtle.forward(100)
+
+turtle.penup()
+
+turtle.color('red', 'yellow')
+
+turtle.pendown()
+
+turtle.left(90)
+
+turtle.forward(100)
+
+turtle.left(180)
+
+turtle.forward(150)
